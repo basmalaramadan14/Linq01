584f9b31ec9effa5132ca1dd390f665f79585fb3
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-04-19 22:24:26 +0200

    Add project files.

---
+++ Linq01/Program.cs
@@ -0,0 +1,24 @@
+﻿using Linq01.DataSources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment
+{
+   
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var seafoodProducts = Source.ProductList
+                .Where(p => p.Category == "Seafood");
+
+            foreach (var product in seafoodProducts)
+            {
+                Console.WriteLine($"{product.ProductName} - {product.UnitPrice}");
+            }
+        }
+    }
+}
